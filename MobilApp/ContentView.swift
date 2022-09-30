97d4c9fdc6a25d500b4bc2ed5d81254d32371a32
nisanur <nisanur>
2022-09-30 08:54:10 +0300

    Initial Commit

---
+++ MobilApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MobilApp
+//
+//  Created by Nisanur Korkmaz on 30/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MobilApp/MobilAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobilAppApp.swift
+//  MobilApp
+//
+//  Created by Nisanur Korkmaz on 30/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MobilAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
