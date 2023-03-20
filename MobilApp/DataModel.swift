595910aca6e9a7bfee6d4ec43b89b9bfdff979b2
Nisanur Korkmaz <80275552+NisanurKorkmaz@users.noreply.github.com>
2023-03-20 17:56:49 +0300

    Add files via upload

---
+++ MobilApp/DataModel.swift
@@ -0,0 +1,16 @@
+//
+//  DataModel.swift
+//  MobilApp
+//
+//  Created by Nisanur Korkmaz on 26/08/2022.
+//
+
+import Foundation
+
+struct Model: Hashable, Codable{
+    let Id : String
+    let Name : String
+    let Lat : Double
+    let Lng : Double
+}
+
